115a81a962f591324c517bdfe91d39f4720a1e7c
viNieL <89811987+Vortix001@users.noreply.github.com>
2024-07-22 07:37:13 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,17 @@
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute() {
+	execute(client) {
 		console.log(`Bot hazır!`);
+    client.user.setStatus("online");
+  const aktiviteler = [
+    "yakında."
+
+  ]
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (aktiviteler.length - 0 + 1) + 0);
+
+    client.user.setActivity(aktiviteler[random], "");
+  }, 2 * 2500);
 	},
 };
